03fed8df2a0c7bba44e543489ed45f7223e82a1e
LC02-15 <LC02-15>
2015-10-30 15:36:57 -0600

    arregle

---
+++ Examen/Assets/Scripts/Cubo_amarillo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubo_amarillo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject,5);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Examen/Assets/Scripts/Cubo_azul.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubo_azul : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
